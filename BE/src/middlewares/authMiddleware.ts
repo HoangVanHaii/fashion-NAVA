f14ab3e8f6c96d4c7ec12d2255c97468f37b208b
Hoàng Văn Hải <hoanggvanhaii0705@gmail.com>
2025-11-20 17:02:35 +0700

    style: fix parameter and update correct payload format

--- BE/src/middlewares/authMiddleware.ts
+++ BE/src/middlewares/authMiddleware.ts
@@ -1,56 +1,45 @@
-// src/middleware/auth.middleware.ts
-
 import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 import { UserJwtPayload } from "../types/request";
 import { AppError } from "../utils/appError";
-// Đã sửa lỗi gõ nhầm 'databasel' -> 'database'
 import { dbPools, getBranchPool } from '../config/database'; 
 
-export const authMiddleware = async (req: Request, res: Response, next: NextFunction) => {
-    // --- PHẦN 1: XÁC THỰC TOKEN ---
-    
-    // const secret = process.env.JWT_SECRET;
-    // if (!secret) {
-    //     console.error('CRITICAL: JWT_SECRET is not defined in .env file.');
-    //     throw new AppError('Internal server configuration error', 500);
-    // }
+export const authMiddleware = async (req: Request, res: Response, next: NextFunction) => {    
+    const secret = process.env.JWT_SECRET;
+    if (!secret) {
+        console.error('CRITICAL: JWT_SECRET is not defined in .env file.');
+        throw new AppError('Internal server configuration error', 500);
+    }
 
-    // const authHeader = req.headers["authorization"];
-    // if (!authHeader || !authHeader.startsWith("Bearer ")) {
-    //     throw new AppError("Token is required", 401);
-    // }
+    const authHeader = req.headers["authorization"];
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+        throw new AppError("Token is required", 401);
+    }
 
-    // const token = authHeader.split(" ")[1];
-    // let decodedPayload: UserJwtPayload;
+    const token = authHeader.split(" ")[1];
+    let decodedPayload: UserJwtPayload;
 
-    // try {
-    //     decodedPayload = jwt.verify(token, secret) as UserJwtPayload;
+    try {
+        decodedPayload = jwt.verify(token, secret) as UserJwtPayload;
         
-    //     if (!decodedPayload.branch_code || !decodedPayload.role) {
-    //         throw new AppError("Invalid token payload: missing 'branch_code' or 'role'", 401);
-    //     }
+        if (!decodedPayload.branch_code || !decodedPayload.role) {
+            throw new AppError("Invalid token payload: missing 'branch_code' or 'role'", 401);
+        }
 
-    //     req.user = decodedPayload;
+        req.user = decodedPayload;
 
-    // } catch (err: any) {
-    //     if (err instanceof jwt.TokenExpiredError) {
-    //         throw new AppError("Token expired", 401);
-    //     }
-    //     throw new AppError("Invalid token", 401);
-    // }
+    } catch (err: any) {
+        if (err instanceof jwt.TokenExpiredError) {
+            throw new AppError("Token expired", 401);
+        }
+        throw new AppError("Invalid token", 401);
+    }
+    const branchCode =  decodedPayload.branch_code;
 
-    // --- PHẦN 2: GÁN KẾT NỐI CSDL ---
-
-    const branchCode = 'DN' // decodedPayload.branch_code;
-
-    // 2. Gán kết nối Chi nhánh
     const branchPool = getBranchPool(branchCode);
     
     if (!branchPool) {
-        // Nếu token có branch_code không hợp lệ (ví dụ 'CENTRAL' hoặc 'ABC')
         console.warn(`[Auth Middleware] Connection failed for invalid branch: ${branchCode}.`);
-        // THROW
         throw new AppError(
             `Service for region ${branchCode} is currently unavailable or invalid.`, 
             503
@@ -62,27 +51,26 @@ export const authMiddleware = async (req: Request, res: Response, next: NextFunc
     next();
 };
 
-/**
- * Middleware kiểm tra role 'employee' (Async)
- */
-export const isemployee = async (req: Request, res: Response, next: NextFunction) => {
+export const isEmployee = async (req: Request, res: Response, next: NextFunction) => {
     const role = req.user?.role;
     
     if (role !== "employee") {
-        // THROW
         throw new AppError('Forbidden: employees only', 403);
     }
     next();
 }
-
-/**
- * Middleware kiểm tra role 'admin' hoặc 'employee' (Async)
- */
-export const adminOremployee = async (req: Request, res: Response, next: NextFunction) => {
+export const isAdmin = async (req: Request, res: Response, next: NextFunction) => {
+    const role = req.user?.role;
+    
+    if (role !== "admin") {
+        throw new AppError('Forbidden: admins only', 403);
+    }
+    next();
+}
+export const adminOrEmployee = async (req: Request, res: Response, next: NextFunction) => {
     const role = req.user?.role;
     
     if (role !== "admin" && role !== "employee") {
-        // THROW
         throw new AppError('Forbidden: Admins or employees only', 403);
     }
     next();
--- BE/src/routers/admin/user.ts
+++ BE/src/routers/admin/user.ts
@@ -2,9 +2,24 @@ import { Router } from 'express';
 import * as admincontroller from '../../controllers/admin/user'
 import * as userMiddleware from '../../middlewares/user'
 import { validateRequest } from '../../middlewares/validateRequest';
+import { authMiddleware, isEmployee, isAdmin } from '../../middlewares/authMiddleware';
 
 const router = Router();
-router.post('/register-employee',userMiddleware.registerEmployee, validateRequest ,admincontroller.registerEmployee);
-router.put('/change-role', userMiddleware.changeRole, validateRequest, admincontroller.changeRole);
+router.post(
+    '/register-employee',
+    authMiddleware,
+    isAdmin,
+    userMiddleware.registerEmployee,
+    validateRequest,
+    admincontroller.registerEmployee
+);
+router.put(
+    '/change-role',
+    authMiddleware,
+    isAdmin,
+    userMiddleware.changeRole,
+    validateRequest,
+    admincontroller.changeRole
+);
 
 export default router;
