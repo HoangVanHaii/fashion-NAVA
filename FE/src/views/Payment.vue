3f8d861a17eda84679fd512411feb4bc59fdd96b
Huy Vui <tranhuyvui714@gmail.com>
2025-12-17 16:55:14 +0700

    delete alert

--- FE/src/views/Payment.vue
+++ FE/src/views/Payment.vue
@@ -327,7 +327,7 @@ const handleOrder = async () => {
         if (selectedPayment.value === 'vnpay' && result.data.paymentUrl) {
             window.location.href = result.data.paymentUrl;
         } else {
-            router.push('/order-success');
+            router.push('/orderSuccess');
         }
     } else {
         showToast(result.message || "Đặt hàng thất bại", false);
